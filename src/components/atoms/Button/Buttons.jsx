e1240eae3a66dacc5472421ee8db804fe4c80a32
Amir Mohsen Jalili <am.jalili@snapp.express>
2023-07-19 12:48:16 +0330

    Added style to the button

--- src/components/atoms/Button/Buttons.jsx
+++ src/components/atoms/Button/Buttons.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './styles.module.scss'
 
 
 const Buttons = ({children, onClick, className}) => {
     return (
         <>
-            <button className={className} onClick={onClick}>{children}</button>
+            <button className={`${styles.button} ${className}`} onClick={onClick}>{children}</button>
         </>
     );
 };
